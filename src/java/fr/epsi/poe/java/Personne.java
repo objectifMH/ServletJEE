8a0083654c0934835b31b79fa3f7ee63d0bc0150
Segolene Bauque <sego.bauque@gmail.com>
2019-09-02 13:44:03 +0200

    first commit

---
+++ src/java/fr/epsi/poe/java/Personne.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.epsi.poe.java;
+
+/**
+ *
+ * @author mjj19
+ */
+public class Personne {
+    private String pseudo;
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+    
+    
+}
